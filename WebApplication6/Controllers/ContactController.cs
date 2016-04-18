93a46a6b5f16395c3feb997dbab43dc31a586615
Jon Coello <joncoello@hotmail.com>
2016-04-18 10:54:17 +0100

    Add project files.

---
+++ WebApplication6/Controllers/ContactController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        private static Models.ContactModel _contact;
+
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail() {
+
+            if (_contact == null)
+            {
+                _contact = new Models.ContactModel()
+                {
+                    ContactID = 123,
+                    FirstName = "Bob",
+                    LastName = "Jones"
+                };
+            }
+            
+            return View(_contact);
+
+        }
+
+        [HttpPost]
+        public ActionResult Detail(Models.ContactModel contact)
+        {
+            _contact = contact;
+            return RedirectToAction("Detail");
+        }
+
+    }
+}
---
+++ WebApplication6/Models/ContactModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6.Models
+{
+    public class ContactModel
+    {
+        [Display(Name = "ID")]
+        public int ContactID { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+    }
+}
